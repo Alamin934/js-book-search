61cbcd5dcdf8f0a2f300ceaf3eb0bbe356324397
Alamin934 <islamalamin934@gmail.com>
2021-09-02 17:52:16 +0600

    last submit

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ const searchBooks = () => {
     fetch(url)
         .then(res => res.json())
         .then(data => displayBooks(data.docs))
+
+    //clear Search Field
     document.getElementById('searchText').value = '';
 };
 
@@ -12,32 +14,33 @@ const searchBooks = () => {
 const displayBooks = books => {
     const bookDetails = document.getElementById('book-details');
     bookDetails.textContent = '';
-    //slice/get only 20 books
-    const bookSliced = books.slice(0, 35);
-    //Total Books Count
-    const totalBooks = document.getElementById('total-books-found');
-    if (bookSliced.length) {
-        totalBooks.innerText = `${bookSliced.length} Results Found`;
-    } else {
-        totalBooks.innerText = `No Result Found`;
-    };
-    //Show Total Books
-    document.getElementById('total-books-found').style.display = 'block';
+    //slice/get Max 35 books
+    const sliceBook = books.slice(0, 35);
 
-    bookSliced.forEach(book => {
+    const totalResultFound = document.getElementById('total-result-found');
+    //Total Books Count and Result Found Condition
+    if (sliceBook.length) {
+        totalResultFound.innerText = `${sliceBook.length} Results Found`;
+    } else {
+        totalResultFound.innerText = `No Result Found`;
+    };
+    //Show Total Book Result
+    document.getElementById('total-result-found').style.display = 'block';
+    //forEach on slice Array
+    sliceBook.forEach(book => {
+        //create div element
         const div = document.createElement('div');
+        //add css class
         div.classList.add('col');
-        const imgNotFound = `https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png`;
-        const coverImg = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
         div.innerHTML = `
             <div class="card h-100">
-                <img src="${coverImg ? coverImg : imgNotFound}" class="card-img-top img-size" alt="...">
-                    <div class="card-body">
-                        <h4 class="card-title">${book.title}</h4>
-                        <p class="card-title fw-bold">Author: <span class="fw-normal">${book.author_name ? book.author_name : 'name not found'}</span></p>
-                        <p class="card-title fw-bold">Publisher: <span class="fw-normal">${book.publisher}</span></p>
-                        <p class="card-title fw-bold">First Publish Year: <span class="fw-normal">${book.first_publish_year}</span></p>
-                    </div>
+                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top img-size" alt="...">
+                <div class="card-body">
+                    <h4 class="card-title">${book.title}</h4>
+                    <p class="card-title fw-bold">Author: <span class="fw-normal">${book.author_name ? book.author_name : 'name not found'}</span></p>
+                    <p class="card-title fw-bold">Publisher: <span class="fw-normal">${book.publisher}</span></p>
+                    <p class="card-title fw-bold">First Publish Year: <span class="fw-normal">${book.first_publish_year}</span></p>
+                </div>
         </div>
         `;
         bookDetails.appendChild(div);
